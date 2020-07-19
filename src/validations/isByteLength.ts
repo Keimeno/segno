269ee606165887698732b69d3ffa81c482c55473
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-19 18:16:10 +0200

    added byte length validation

---
+++ src/validations/isByteLength.ts
@@ -0,0 +1,30 @@
+import { assertString } from '../util/assertString.ts';
+
+type ByteLengthOptions = {
+  /**
+   * @default 0
+   */
+  min?: number;
+
+  /**
+   * @default undefined
+   */
+  max?: number;
+};
+
+const defaultByteLengthOptions: ByteLengthOptions = {
+  min: 0,
+};
+
+export const isByteLength = (str: string, options?: ByteLengthOptions) => {
+  assertString(str);
+
+  options = { ...defaultByteLengthOptions, ...options };
+  let { min, max } = options;
+
+  // force min to be 0 if undefined
+  min = min || 0;
+
+  const len = encodeURI(str).split(/%..|./).length - 1;
+  return len >= min && (typeof max === 'undefined' || len <= max);
+};
