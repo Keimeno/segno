9d24118272c8e85fdab1f5cd254f115bad2ff13f
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-20 18:34:25 +0200

    renamed util to helpers

--- src/util/assertString.ts
+++
@@ -1,7 +0,0 @@
-export const assertString = (input: string) => {
-  const isString = typeof input === 'string';
-
-  if (!isString) {
-    throw new TypeError(`Expected a string but received ${typeof input}.`);
-  }
-};
