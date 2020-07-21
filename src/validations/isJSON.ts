17f5172a0df400d3cf471cf2e38c538522703d13
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:05:54 +0200

    added is json validation

---
+++ src/validations/isJSON.ts
@@ -0,0 +1,27 @@
+import { assertString } from '../helpers/assertString.ts';
+
+type JSONOptions = {
+  allowPrimitives?: boolean;
+};
+
+const defaultJSONOptions = {
+  allowPrimitives: false,
+};
+
+export const isJSON = (str: string, options?: JSONOptions) => {
+  assertString(str);
+
+  try {
+    options = { ...defaultJSONOptions, ...options };
+    let primitives: Array<boolean | null> = [];
+    if (options.allowPrimitives) {
+      primitives = [null, false, true];
+    }
+
+    const obj = JSON.parse(str);
+    return primitives.includes(obj) || (!!obj && typeof obj === 'object');
+  } catch (e) {
+    /* ignore */
+  }
+  return false;
+};
