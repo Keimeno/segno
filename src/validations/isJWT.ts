1505df09e9f0e6c6dad3868e53e4a225fc074800
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:06:02 +0200

    added is jwt validation

---
+++ src/validations/isJWT.ts
@@ -0,0 +1,18 @@
+import { assertString } from '../helpers/assertString.ts';
+import isBase64 from './isBase64.ts';
+
+export const isJWT = (str: string) => {
+  assertString(str);
+
+  const dotSplit = str.split('.');
+  const len = dotSplit.length;
+
+  if (len > 3 || len < 2) {
+    return false;
+  }
+
+  return dotSplit.reduce(
+    (acc, currElem) => acc && isBase64(currElem, { urlSafe: true }),
+    true
+  );
+};
