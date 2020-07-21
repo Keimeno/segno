a33593076b6154c479bb238dbaa2f27cae01915d
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:10:59 +0200

    added is uuid validation

---
+++ src/validations/isUUID.ts
@@ -0,0 +1,14 @@
+import { assertString } from '../helpers/assertString.ts';
+
+const uuid = {
+  3: /^[0-9A-F]{8}-[0-9A-F]{4}-3[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+  4: /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+  5: /^[0-9A-F]{8}-[0-9A-F]{4}-5[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+  all: /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+};
+
+export const isUUID = (str: string, version: 3 | 4 | 5 | 'all' = 'all') => {
+  assertString(str);
+  const pattern = (uuid as any)[version];
+  return pattern && pattern.test(str);
+};
