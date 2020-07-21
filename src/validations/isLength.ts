3b25597fbd003a55afce5b52a148fceb0d60ef8a
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:06:29 +0200

    added is length validation

---
+++ src/validations/isLength.ts
@@ -0,0 +1,17 @@
+import { assertString } from '../helpers/assertString.ts';
+
+type LengthOptions = {
+  min?: number;
+  max?: number;
+};
+
+export const isLength = (str: string, options?: LengthOptions) => {
+  assertString(str);
+
+  const min = options?.min || 0;
+  const max = options?.max;
+
+  const surrogatePairs = str.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g) || [];
+  const len = str.length - surrogatePairs.length;
+  return len >= min && (typeof max === 'undefined' || len <= max);
+};
