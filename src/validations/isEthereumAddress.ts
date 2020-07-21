65365ef004cf501b106233c499360b0f7279e128
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:01:33 +0200

    added is ethereum address validation

---
+++ src/validations/isEthereumAddress.ts
@@ -0,0 +1,8 @@
+import { assertString } from '../helpers/assertString.ts';
+
+const eth = /^(0x)[0-9a-f]{40}$/i;
+
+export const isEthereumAddress = (str: string) => {
+  assertString(str);
+  return eth.test(str);
+};
