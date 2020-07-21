b51bca037158839f56859f835a9d743740b833c2
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:02:39 +0200

    added is hexadecimal validation

---
+++ src/validations/isHexadecimal.ts
@@ -0,0 +1,8 @@
+import { assertString } from '../helpers/assertString.ts';
+
+const hexadecimal = /^(0x|0h)?[0-9A-F]+$/i;
+
+export const isHexadecimal = (str: string) => {
+  assertString(str);
+  return hexadecimal.test(str);
+};
