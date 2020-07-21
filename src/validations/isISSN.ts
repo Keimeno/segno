5700cb773ba1b65387f44eed3494a7243faa2fa0
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:05:01 +0200

    added is issn validation

---
+++ src/validations/isISSN.ts
@@ -0,0 +1,30 @@
+import { assertString } from '../helpers/assertString.ts';
+
+const issn = '^\\d{4}-?\\d{3}[\\dX]$';
+
+type ISSNOptions = {
+  requireHyphen?: boolean;
+  caseSensitive?: boolean;
+};
+
+export const isISSN = (str: string, options: ISSNOptions = {}) => {
+  assertString(str);
+  let testIssn = issn;
+
+  testIssn = options.requireHyphen ? testIssn.replace('?', '') : testIssn;
+  const testIssnRegex = options.caseSensitive
+    ? new RegExp(testIssn)
+    : new RegExp(testIssn, 'i');
+
+  if (!testIssnRegex.test(str)) {
+    return false;
+  }
+
+  const digits = str.replace('-', '').toUpperCase();
+  let checksum = 0;
+  for (let i = 0; i < digits.length; i++) {
+    const digit = digits[i];
+    checksum += (digit === 'X' ? 10 : +digit) * (8 - i);
+  }
+  return checksum % 11 === 0;
+};
