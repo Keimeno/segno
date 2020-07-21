7d0b05ac878342b3025e2d249680ee8ee31f430a
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:08:31 +0200

    added is multibyte validation

---
+++ src/validations/isMultibyte.ts
@@ -0,0 +1,8 @@
+import { assertString } from '../helpers/assertString.ts';
+
+const multibyte = /[^\x00-\x7F]/;
+
+export const isMultibyte = (str: string) => {
+  assertString(str);
+  return multibyte.test(str);
+};
