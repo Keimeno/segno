5735e06004d5d31845bfd140cae8a64f4aca5a06
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:06:44 +0200

    added is locale validation

---
+++ src/validations/isLocale.ts
@@ -0,0 +1,13 @@
+import { assertString } from '../helpers/assertString.ts';
+
+const localeReg = /^[A-z]{2,4}([_-]([A-z]{4}|[\d]{3}))?([_-]([A-z]{2}|[\d]{3}))?$/;
+
+export const isLocale = (str: string) => {
+  assertString(str);
+
+  if (str === 'en_US_POSIX' || str === 'ca_ES_VALENCIA') {
+    return true;
+  }
+
+  return localeReg.test(str);
+};
