9437ccba43551ef92c8cdf6048b561bdbb742523
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:09:55 +0200

    added is rgb color validation

---
+++ src/validations/isRgbColor.ts
@@ -0,0 +1,21 @@
+import { assertString } from '../helpers/assertString.ts';
+
+const rgbColor = /^rgb\((([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]),){2}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\)$/;
+const rgbaColor = /^rgba\((([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]),){3}(0?\.\d|1(\.0)?|0(\.0)?)\)$/;
+const rgbColorPercent = /^rgb\((([0-9]%|[1-9][0-9]%|100%),){2}([0-9]%|[1-9][0-9]%|100%)\)/;
+const rgbaColorPercent = /^rgba\((([0-9]%|[1-9][0-9]%|100%),){3}(0?\.\d|1(\.0)?|0(\.0)?)\)/;
+
+export const isRgbColor = (str: string, includePercentValues = true) => {
+  assertString(str);
+
+  if (!includePercentValues) {
+    return rgbColor.test(str) || rgbaColor.test(str);
+  }
+
+  return (
+    rgbColor.test(str) ||
+    rgbaColor.test(str) ||
+    rgbColorPercent.test(str) ||
+    rgbaColorPercent.test(str)
+  );
+};
