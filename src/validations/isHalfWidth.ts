8169a9407f7b27b1de0fd0a8c001288f2b524449
Constantin Metz <58604248+Keimeno@users.noreply.github.com>
2020-07-21 19:01:55 +0200

    added is half width validation

---
+++ src/validations/isHalfWidth.ts
@@ -0,0 +1,8 @@
+import { assertString } from '../helpers/assertString.ts';
+
+export const halfWidth = /[\u0020-\u007E\uFF61-\uFF9F\uFFA0-\uFFDC\uFFE8-\uFFEE0-9a-zA-Z]/;
+
+export const isHalfWidth = (str: string) => {
+  assertString(str);
+  return halfWidth.test(str);
+};
