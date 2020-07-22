5a4b52afa5f9b3e1af808913ea20cc0172e18015
Constantin Metz <constantin@metzworld.com>
2020-07-22 22:47:28 +0200

    added is int test

---
+++ tests/validations/isInt.test.ts
@@ -0,0 +1,65 @@
+import { test } from '../index.ts';
+
+Deno.test('should validate integers', () => {
+  test({
+    validator: 'isInt',
+    valid: ['13', '123', '0', '123', '-0', '+1', '01', '-01', '000'],
+    invalid: ['100e10', '123.123', '   ', ''],
+  });
+  test({
+    validator: 'isInt',
+    args: [{ allowLeadingZeroes: false }],
+    valid: ['13', '123', '0', '123', '-0', '+1'],
+    invalid: ['01', '-01', '000', '100e10', '123.123', '   ', ''],
+  });
+  test({
+    validator: 'isInt',
+    args: [{ allowLeadingZeroes: true }],
+    valid: [
+      '13',
+      '123',
+      '0',
+      '123',
+      '-0',
+      '+1',
+      '01',
+      '-01',
+      '000',
+      '-000',
+      '+000',
+    ],
+    invalid: ['100e10', '123.123', '   ', ''],
+  });
+  test({
+    validator: 'isInt',
+    args: [
+      {
+        min: 10,
+      },
+    ],
+    valid: ['15', '80', '99'],
+    invalid: ['9', '6', '3.2', 'a'],
+  });
+  test({
+    validator: 'isInt',
+    args: [
+      {
+        min: 10,
+        max: 15,
+      },
+    ],
+    valid: ['15', '11', '13'],
+    invalid: ['9', '2', '17', '3.2', '33', 'a'],
+  });
+  test({
+    validator: 'isInt',
+    args: [
+      {
+        gt: 10,
+        lt: 15,
+      },
+    ],
+    valid: ['14', '11', '13'],
+    invalid: ['10', '15', '17', '3.2', '33', 'a'],
+  });
+});
