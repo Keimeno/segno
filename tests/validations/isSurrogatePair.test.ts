09fbb06e09b22a7358580bdc053817572bde7969
Constantin Metz <constantin@metzworld.com>
2020-07-22 23:12:38 +0200

    added is surrogate pair tests

---
+++ tests/validations/isSurrogatePair.test.ts
@@ -0,0 +1,9 @@
+import { test } from '../index.ts';
+
+Deno.test('should validate surrogate pair strings', () => {
+  test({
+    validator: 'isSurrogatePair',
+    valid: ['𠮷野𠮷', '𩸽', 'ABC千𥧄1-2-3'],
+    invalid: ['吉野竈', '鮪', 'ABC1-2-3'],
+  });
+});
