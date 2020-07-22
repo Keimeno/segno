408baf49ccd866e50efd42bab1a982ad2b1d760a
Constantin Metz <constantin@metzworld.com>
2020-07-22 23:01:00 +0200

    added is before tests and fixed boolean cast

--- lib/validations/isBefore.ts
+++ lib/validations/isBefore.ts
@@ -7,5 +7,5 @@ export const isBefore = (str: string, date = String(new Date())) => {
   const comparison = toDate(date);
   const original = toDate(str);
 
-  return (original && comparison && original < comparison) as boolean;
+  return !!(original && comparison && original < comparison);
 };
