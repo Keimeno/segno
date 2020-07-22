00994f1db4d85af87cbd46417a35fad4eb9cdefd
Constantin Metz <constantin@metzworld.com>
2020-07-22 23:27:25 +0200

    added is iso31661 alpha 3 tests

---
+++ tests/validations/isISO31661Alpha3.test.ts
@@ -0,0 +1,10 @@
+import { test } from '../index.ts';
+
+Deno.test('should validate ISO 3166-1 alpha 3 country codes', () => {
+  // from https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+  test({
+    validator: 'isISO31661Alpha3',
+    valid: ['ABW', 'HND', 'KHM', 'RWA'],
+    invalid: ['', 'FR', 'fR', 'GB', 'PT', 'CM', 'JP', 'PM', 'ZW'],
+  });
+});
