9492ddbcd43d65ca7776902b1120e48f85fe7ab1
Constantin Metz <constantin@metzworld.com>
2020-07-22 23:13:43 +0200

    added is base 32 tests

---
+++ tests/validations/isBase32.test.ts
@@ -0,0 +1,27 @@
+import { test } from '../index.ts';
+
+Deno.test('should validate base32 strings', () => {
+  test({
+    validator: 'isBase32',
+    valid: [
+      'ZG======',
+      'JBSQ====',
+      'JBSWY===',
+      'JBSWY3A=',
+      'JBSWY3DP',
+      'JBSWY3DPEA======',
+      'K5SWYY3PNVSSA5DPEBXG6ZA=',
+      'K5SWYY3PNVSSA5DPEBXG6===',
+    ],
+    invalid: [
+      '12345',
+      '',
+      'JBSWY3DPtesting123',
+      'ZG=====',
+      'Z======',
+      'Zm=8JBSWY3DP',
+      '=m9vYg==',
+      'Zm9vYm/y====',
+    ],
+  });
+});
