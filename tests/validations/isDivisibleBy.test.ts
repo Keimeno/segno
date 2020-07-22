b01250e60393e5178ab6e24640f23dc855c18aa6
Constantin Metz <constantin@metzworld.com>
2020-07-22 23:02:53 +0200

    added is divisible by tests

---
+++ tests/validations/isDivisibleBy.test.ts
@@ -0,0 +1,13 @@
+import { test } from '../index.ts';
+
+Deno.test(
+  'should validate that integer strings are divisible by a number',
+  () => {
+    test({
+      validator: 'isDivisibleBy',
+      args: [2],
+      valid: ['2', '4', '100', '1000'],
+      invalid: ['1', '2.5', '101', 'foo', '', '2020-01-06T14:31:00.135Z'],
+    });
+  }
+);
